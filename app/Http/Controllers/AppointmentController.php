cf0370730a18ab5fee48f8b69eec201c067aa86b
John Dale Villanueva <johndalevillanueva72@gmail.com>
2024-03-20 18:13:08 +0800

    added create, edit, delete modal

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -24,6 +24,9 @@ class AppointmentController extends Controller
         return view('appointment.schedule');
     }
 
+    
+   
+
     public function store(Request $request)
     {
         $data = $request->validate([
@@ -39,5 +42,32 @@ class AppointmentController extends Controller
         $NewAppointment = Appointment::create($data);
         return redirect(route('appointment.index'))->with('success', 'Appointment created successfully!');
     }
+
+    public function edit(Appointment $appointment)
+{
+    return view('appointment.edit', compact('appointment'));
+}
+
+    
+
+    public function update (Appointment $appointment, Request $request){
+        $data = $request->validate([
+            'service_name' => 'required|string|max:100',
+            'service_type' => 'required|string|max:100',
+            'office' => 'required|string|max:100',
+            'status' => 'required|string|max:100',
+            
+        ]);
+
+        $appointment->update($data);
+        return redirect(route('appointment.index'))->with('success', 'Appointment updated Successfully');
+    }
+
+    public function destroy(Appointment $appointment){
+        $appointment->delete();
+        return redirect(route('appointment.index'))->with('success', 'Appointment deleted Successfully');
+
+    }
+    
 }
 
