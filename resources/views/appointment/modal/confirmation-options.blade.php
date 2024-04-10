feadd17c487c042e60bfd86fbf3f461dd1a8b1b3
DEL <rodeldeleon199718@gmail.com>
2024-04-11 07:49:34 +0800

    Added appointment modal views

---
+++ resources/views/appointment/modal/confirmation-options.blade.php
@@ -0,0 +1,17 @@
+<div class="modal" id="confirmationOptions">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Confirmation</h5>
+                <button type="button" class="btn-close" aria-label="Close" onclick="closeConfirmationOptions()"></button>
+            </div>
+            <div class="modal-body">
+                <p>Are you sure you want to proceed with the appointment?</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" onclick="closeConfirmationOptions()">Cancel</button>
+                <button type="button" class="btn btn-primary" id="close" onclick="close()">Confirm</button>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ resources/views/appointment/modal/success.blade.php
@@ -0,0 +1,16 @@
+<div class="modal" id="success">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Confirmation</h5>
+                <button type="button" class="btn-close" aria-label="Close" onclick="closeSuccess()"></button>
+            </div>
+            <div class="modal-body">
+                <p>Appointment Successfully Saved</p>
+            </div>
+            <div class="modal-footer">
+
+            </div>
+        </div>
+    </div>
+</div>
