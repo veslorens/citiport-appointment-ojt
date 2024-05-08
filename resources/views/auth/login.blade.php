543f70ad1a1e23466d5a81c83a67d4c08f5beb5f
DELcdsf <rodeldeleon199718@gmail.com>
2024-05-08 09:08:32 +0800

    update database

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,59 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Login') }}</div>
+
+                <div class="card-body">
+                    <form method="POST" action="{{ route('login') }}">
+                        @csrf
+
+                        <div class="mb-3 row">
+                            <label for="email_or_name" class="col-md-4 col-form-label text-md-end">{{ __('Email Address or Name') }}</label>
+                            <div class="col-md-6">
+                                <input id="email_or_name" type="text" class="form-control @error('email_or_name') is-invalid @enderror" name="email_or_name" value="{{ old('email_or_name') }}" required autocomplete="email_or_name" autofocus>
+                                @error('email_or_name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="mb-3 row">
+                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="mb-3 row">
+                            <div class="col-md-6 offset-md-4 d-flex align-items-center justify-content-between">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                    <label class="form-check-label" for="remember">{{ __('Remember Me') }}</label>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="mb-0 row">
+                            <div class="col-md-8 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Login') }}
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
